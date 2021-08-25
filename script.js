07116ca3d4570d7c964c171d4a56a30cb1ffb93b
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-08-25 19:17:47 -0300

    implementaçao do slider

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-/* script */
+$(document).ready(function(){
+  $('#slider').bxSlider({
+      auto: true,
+      controls: false,
+  });
+});
+
+const buscaClasses = async () => {
+  const response = await fetch('https://www.dnd5eapi.co/api/classes');
+  return (await response.json()).results;
+}
+buscaClasses();
+const selectClasses = document.getElementById('classes');
