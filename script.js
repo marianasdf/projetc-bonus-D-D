59d039232237b3f28b58efe22c7ca9134c6354b5
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-08-26 01:22:18 -0300

    implementazao da API e funçao montaListaDeClasses

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('#slider').bxSlider({
-      auto: true,
-      controls: false,
+    auto: true,
+    controls: false,
   });
 });
 
@@ -9,5 +9,22 @@ const buscaClasses = async () => {
   const response = await fetch('https://www.dnd5eapi.co/api/classes');
   return (await response.json()).results;
 }
-buscaClasses();
-const selectClasses = document.getElementById('classes');
+
+const ul = document.getElementById("classes")
+
+const montaListaDeClasses = async () => {
+  const classes = await buscaClasses();
+  classes.forEach((classe) => {
+    const li = document.createElement('li');
+    const anchor = document.createElement('a');
+    anchor.innerText = classe.name;
+    anchor.className = 'dropdown-item'
+    anchor.href = '#';
+    li.appendChild(anchor);
+    ul.appendChild(li);
+  });
+}
+
+window.onload = async () => {
+  await montaListaDeClasses();
+}
